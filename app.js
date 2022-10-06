75d2e0e791039f96efc5380275a588c309707027
miaout11 <miaout.11@gmail.com>
2022-10-06 16:23:48 +0800

    feat: add mongoose

--- app.js
+++ app.js
@@ -2,6 +2,20 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose') // 載入 mongoose
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true }) // 設定連線到 mongoDB
+
+// 取得資料庫連線狀態
+const db = mongoose.connection
+// 連線異常
+db.on('error', () => {
+  console.log('mongodb error!')
+})
+// 連線成功
+db.once('open', () => {
+  console.log('mongodb connected!')
+})
+
 // 設定首頁路由
 app.get('/', (req, res) => {
   res.send('hello world')
