643508e4e1a98666cfa311942e85c3c626ed3409
miaout11 <miaout.11@gmail.com>
2022-10-07 16:51:35 +0800

    feat: add update todo function

--- app.js
+++ app.js
@@ -18,16 +18,16 @@ db.once('open', () => {
   console.log('mongodb connected!')
 })
 // setting template engine
-app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs'}))
+app.engine('hbs', exphbs({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', 'hbs')
 // setting body-parser
 // 只有使用者的請求先經過body-parser的解讀後，我們才能在req.body中取得表單傳送過來的資料。
 app.use(express.urlencoded({ extended: true }))
 
-// setting all todo's route
+// setting reading all todo's route
 app.get('/', (req, res) => {
   Todo.find() // 叫Todo model去資料庫查找出資料
-  // 撈資料以後想用 res.render()，要先用 .lean() 來處理
+    // 撈資料以後想用 res.render()，要先用 .lean() 來處理
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
     .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index(前端) 樣板
     .catch(error => console.error(error)) // 如果發生意外，執行錯誤處理
@@ -36,21 +36,37 @@ app.get('/', (req, res) => {
 app.get('/todos/new', (req, res) => {
   return res.render('new')
 })
-// setting create todo route
 app.post('/todos', (req, res) => {
   const name = req.body.name // 從 req.body 拿出表單裡的 name 資料
   return Todo.create({ name }) // call Todo 直接新增資料
-  .then(() => res.redirect('/') )
-  .catch(error => console.error(error))
+    .then(() => res.redirect('/'))
+    .catch(error => console.error(error))
 })
-// setting detail route
+// setting detail page 使用動態參數":"，可以用req.params取出
 app.get('/todos/:id', (req, res) => {
-  const id = req.params.id
-  return Todo.findById(id)
+  return Todo.findById(req.params.id) // 從資料庫找出特定一筆todo資料(使用findById)並用req.params取出資料
+    .lean() // 把資料轉換成單純的JS物件
+    .then(todo => res.render('detail', { todo })) // 將資料傳給 detail(前端) 樣板
+    .catch(error => console.error(error))
+})
+// setting edit page
+app.get('/todos/:id/edit', (req, res) => {
+  return Todo.findById(req.params.id)
     .lean()
-    .then(todo => res.render('detail', { todo }))
-    .catch(error => console.error(error)) 
-}) 
+    .then(todo => res.render('edit', { todo }))
+    .catch(error => console.error(error))
+})
+app.post('/todos/:id/edit', (req, res) => {
+  const id = req.params.id
+  const name = req.body.name
+  return Todo.findById(id)
+    .then(todo => {
+      todo.name = name
+      return todo.save()
+    })
+    .then(() => res.redirect(`/todos/${id}`))
+    .catch(error => console.error(error))
+})
 
 // setting port 3000
 app.listen(3000, () => {
