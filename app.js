d63143cc3995002931c8a05a44b8c568e9011dec
miaout11 <miaout.11@gmail.com>
2022-10-11 18:03:02 +0800

    feat: add sort functon

--- app.js
+++ app.js
@@ -31,7 +31,8 @@ app.get('/', (req, res) => {
   Todo.find() // 叫Todo model去資料庫查找出資料
     // 撈資料以後想用 res.render()，要先用 .lean() 來處理
     .lean() // 把 Mongoose 的 Model 物件轉換成乾淨的 JavaScript 資料陣列
-    .then(todos => res.render('index', { todos: todos })) // 將資料傳給 index(前端) 樣板
+    .sort({ _id: 'asc' }) // 根據 _id 升冪排序 desc則是降冪排序
+    .then(todos => res.render('index', { todos })) // 將資料傳給 index(前端) 樣板
     .catch(error => console.error(error)) // 如果發生意外，執行錯誤處理
 })
 // setting create's route
