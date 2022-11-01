f893377fa82d97e31b0584f16499c3d7abedf3a3
miaout11 <miaout.11@gmail.com>
2022-11-01 15:26:52 +0800

    feat: add user login page

--- routes/modules/users.js
+++ routes/modules/users.js
@@ -1,8 +1,17 @@
-const express = require('express') // 載入express
-const router = express.Router() // 載入express router
+const express = require('express') // use express
+const router = express.Router() // use express router
 
+// login page
 router.get('/login', (req, res) => {
   res.render('login')
+})
+// login data
+router.post('/login', (req, res) => {
+
+})
+// register page
+router.get('/register', (req, res) => {
+  res.render('register')
 })
 
 module.exports = router
