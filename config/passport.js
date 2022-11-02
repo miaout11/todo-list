fcab536f572e099b58d6e22f4fb2efdf0e81db30
miaout11 <miaout.11@gmail.com>
2022-11-02 22:41:48 +0800

    add bcrypt to login route

--- config/passport.js
+++ config/passport.js
@@ -1,4 +1,5 @@
 const passport = require('passport')
+const bcrypt = require('bcryptjs')
 const LocalStratege = require('passport-local').Strategy
 
 const User = require('../models/user')
@@ -14,10 +15,13 @@ module.exports = app => {
         if (!user) {
           return done(null, false, req.flash('warning_msg', 'This email is not registered!'))
         }
-        if (user.password !== password) {
-          return done(null, false, req.flash('warning_msg', 'Email or Password incorrect.'))
-        }
-        return done(null, user)
+        // bcrypt.compare(password, user.password) 第一個參數是使用者的輸入值，而第二個參數是資料庫裡的雜湊值，bcrypt 會幫我們做比對，並回傳布林值
+        return bcrypt.compare(password, user.password).then(isMatch => {
+          if (!isMatch) {
+            return done(null, false, req.flash('warning_msg', 'Email or Password incorrect.'))
+          }
+          return done(null, user)
+        })
       })
       .catch(err => done(err, false))
   }))
